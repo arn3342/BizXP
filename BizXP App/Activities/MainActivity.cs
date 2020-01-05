ee77fe6b43c6ecba82f95be72160279bd9006d97
Aousaf Rashisd <nabilrashid44@gmail.com>
2020-01-05 21:16:54 +0600

    new UI Updates

--- BizXP App/Activities/MainActivity.cs
+++ BizXP App/Activities/MainActivity.cs
@@ -4,19 +4,31 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 using Android.Content.PM;
+using Android.Content;
 
-namespace BizXP_App
+namespace BizXP_App.Activities
 {
     [Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/Theme.AppBlueTheme", WindowSoftInputMode = Android.Views.SoftInput.AdjustPan, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : AppCompatActivity
     {
+        private Button inventoryBtn;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            inventoryBtn = FindViewById<Button>(Resource.Id.inventoryBtn);
+            inventoryBtn.Click += InventoryBtn_Click;
         }
+
+        private void InventoryBtn_Click(object sender, System.EventArgs e)
+        {
+            Intent inventoryIntent = new Intent(this, typeof(InventoryActivity));
+            StartActivity(inventoryIntent);
+    }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
